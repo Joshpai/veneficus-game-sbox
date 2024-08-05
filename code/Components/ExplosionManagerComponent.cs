d165b7a90de905af3a55024b601d9ea0dc0f7b82
Joshpai <Joshpai@users.noreply.github.com>
2024-08-05 13:54:53 +0100

    feat: add explosion knockback

--- code/Components/ExplosionManagerComponent.cs
+++ code/Components/ExplosionManagerComponent.cs
@@ -9,6 +9,9 @@ public sealed class ExplosionManagerComponent : Component
 	[Property]
 	public float ExplosionDamage { get; set; } = 20.0f;
 
+	[Property]
+	public float ImpulseForce { get; set; } = 10000.0f;
+
 	public float DamageMultiplier { get; set; } = 1.0f;
 
 	public void Explode()
@@ -30,10 +33,18 @@ public sealed class ExplosionManagerComponent : Component
 			if (!hit.Hit)
 				continue;
 
+			Vector3 explosionToHit = hit.GameObject.Transform.Position
+								   - ExplosionOrigin;
+
 			var hp = hit.GameObject.Components
 								   .GetInDescendantsOrSelf<HealthComponent>();
 			if (hp != null)
 				hp.Damage(ExplosionDamage * DamageMultiplier);
+
+			var rigidBody = hit.GameObject.Components
+										  .GetInDescendantsOrSelf<Rigidbody>();
+			if (rigidBody != null)
+				rigidBody.ApplyImpulse(explosionToHit * ImpulseForce);
 		}
 	}
 }
