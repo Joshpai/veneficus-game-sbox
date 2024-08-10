cb22a36c7789ae200482a7ed82c36a50b8b4f1fc
Joshpai <Joshpai@users.noreply.github.com>
2024-08-10 21:07:59 +0100

    feat: spawn player on info_player_spawn

---
+++ code/Components/MapPlayerSpawner.cs
@@ -0,0 +1,49 @@
+public sealed class MapPlayerSpawner : Component
+{
+	private MapInstance _map;
+
+	protected override void OnEnabled()
+	{
+		if (this == null)
+			return;
+
+		_map = Components.Get<MapInstance>();
+
+		if (_map != null)
+		{
+			_map.OnMapLoaded += SpawnPlayer;
+
+			if (_map.IsLoaded)
+			{
+				SpawnPlayer();
+			}
+		}
+	}
+
+	protected override void OnDisabled()
+	{
+		if (_map != null)
+		{
+			_map.OnMapLoaded -= SpawnPlayer;
+		}
+	}
+
+	void SpawnPlayer()
+	{
+		var player = new GameObject();
+		player.SetPrefabSource("prefabs/player.prefab");
+		player.UpdateFromPrefab();
+
+		var controller = player.Components.Get<PlayerMovementController>();
+		if (controller == null)
+			return;
+
+		var spawnPoints = Scene.GetAllComponents<SpawnPoint>();
+		foreach (var spawn in spawnPoints)
+		{
+		    controller.Transform.Position = spawn.Transform.Position;
+		    controller.EyeAngles = spawn.Transform.Rotation.Angles();
+			break;
+		}
+	}
+}
